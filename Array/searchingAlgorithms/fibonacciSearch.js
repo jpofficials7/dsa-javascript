9037117991ce5542bd66c48eb8db90baf7925c54
jpofficials7 <jpofficials7@gmail.com>
2024-06-03 10:30:44 +0530

    Fibonacci Search

--- Array/searchingAlgorithms/fibonacciSearch.js
+++ Array/searchingAlgorithms/fibonacciSearch.js
@@ -3,6 +3,14 @@
 // 0 1 1 2 3 5 8 13 21 34 55 89
 
 // Similarities With Binary Search
+
 // 1. Works for sorted Arrays
 // 2. A Divide and Conquer Algorithm
 // 3. Has log n time complexity
+
+// Differences with Binary Search
+
+// 1. Fibonacci Search divides given array in unequal parts
+// 2. Binary Search uses division operator to divide range. Fibonacci Search doesn't use / , but uses + and -. The division operator   may be costly on some CPUs.
+// 3. Fibonacci Search examines relatively closer elements in subsequent steps. So when input array is big that can not fit in CPU cache or even in RAM, Fibonacci Search can be useful.
+
