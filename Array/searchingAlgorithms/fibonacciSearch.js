b2ac601551b2ff2ae808214e5712292e7e57d8db
jpofficials7 <jpofficials7@gmail.com>
2024-06-03 10:17:43 +0530

    fibonacci search

---
+++ Array/searchingAlgorithms/fibonacciSearch.js
@@ -0,0 +1,8 @@
+// Definition: Fibonacci Search is a comparison based technique that uses Fibonacci numbers to search an element in a sorted array.
+
+// 0 1 1 2 3 5 8 13 21 34 55 89
+
+// Similarities With Binary Search
+// 1. Works for sorted Arrays
+// 2. A Divide and Conquer Algorithm
+// 3. Has log n time complexity
