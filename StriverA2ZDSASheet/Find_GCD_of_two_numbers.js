ae8bf8c6527c02026c0abbb46bfe251219333c3a
jpofficials7 <jpofficials7@gmail.com>
2025-07-25 14:28:26 +0530

    Find_GCD_of_two_numbers

--- StriverA2ZDSASheet/Find_GCD_of_two_numbers.js
+++ StriverA2ZDSASheet/Find_GCD_of_two_numbers.js
@@ -37,3 +37,13 @@ function findGcd(n1, n2) {
 }
 
 console.log(findGcd(9, 12));
+
+function findGcdOptimalApproach(n1, n2) {
+  for (let i = Math.min(n1, n2); i > 0; i--) {
+    if (n1 % i === 0 && n2 % i === 0) {
+      return i;
+    }
+  }
+}
+
+console.log(findGcdOptimalApproach(9, 12));
