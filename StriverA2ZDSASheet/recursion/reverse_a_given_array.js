d1df22d24691f35d75d04c275701105a05a4ea5d
jpofficials7 <jpofficials7@gmail.com>
2025-07-29 21:59:39 +0530

    reverseArrayRecursive

--- StriverA2ZDSASheet/recursion/reverse_a_given_array.js
+++ StriverA2ZDSASheet/recursion/reverse_a_given_array.js
@@ -1,10 +1,38 @@
-function reverseArrayUsingExtraArray(arr) {
-  const revArray = [];
-  for (let i = arr.length - 1; i >= 0; i--) {
-    revArray.push(arr[i]);
+// function reverseArrayUsingExtraArray(arr) {
+//   const revArray = [];
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     revArray.push(arr[i]);
+//   }
+//   console.log(revArray);
+// }
+
+// reverseArrayUsingExtraArray(arr);
+
+// function reverseArray(arr) {
+//   let p1 = 0;
+//   let p2 = arr.length - 1;
+//   while (p1 < p2) {
+//     let temp = arr[p1];
+//     arr[p1] = arr[p2];
+//     arr[p2] = temp;
+//     p1++;
+//     p2--;
+//   }
+//   console.log(arr);
+// }
+
+// reverseArray(arr);
+
+function reverseArrayRecursive(arr, start, end) {
+  if (start < end) {
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    reverseArrayRecursive(arr, start + 1, end - 1);
   }
-  console.log(revArray);
 }
 
 let arr = [1, 2, 3, 4, 5];
-reverseArrayUsingExtraArray(arr);
+
+reverseArrayRecursive(arr, 0, 4);
+console.log(arr);
