87ca9b4f954bab010ecfdf122246ac20ad345dc4
jpofficials7 <jpofficials7@gmail.com>
2025-08-03 16:28:46 +0530

    bubble sort optimized

--- StriverA2ZDSASheet/sorting/bubbleSort.js
+++ StriverA2ZDSASheet/sorting/bubbleSort.js
@@ -4,13 +4,16 @@
 
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
+    let didSwap = 0;
     for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        didSwap = 1;
       }
     }
+    if (didSwap === 0) break;
   }
 }
 
