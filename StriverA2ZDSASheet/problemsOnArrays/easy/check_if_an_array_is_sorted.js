ed90c53c77a913c5444cfd11cafae3f246e9968f
jpofficials7 <jpofficials7@gmail.com>
2025-08-09 14:16:42 +0530

    check if an array is sorted

---
+++ StriverA2ZDSASheet/problemsOnArrays/easy/check_if_an_array_is_sorted.js
@@ -0,0 +1,26 @@
+function isSorted(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const arr = [1, 2, 3, 4, 5];
+const ans = isSorted(arr);
+if (ans) console.log('True');
+else console.log('false');
+
+function isSortedOptimal(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isSorted(arr) ? 'True' : 'False');
