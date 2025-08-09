03abf4be59aad9d59e464e015bda8537c286552b
jpofficials7 <jpofficials7@gmail.com>
2025-08-09 13:16:38 +0530

    find second smallest and second largest element in the array

--- StriverA2ZDSASheet/problemsOnArrays/easy/find_second_smallest_and_second_largest_element_in_an_array.js
+++ StriverA2ZDSASheet/problemsOnArrays/easy/find_second_smallest_and_second_largest_element_in_an_array.js
@@ -83,3 +83,69 @@ function getElements(arr) {
 getElements(arr);
 
 //#endregion
+
+//#region - Optimal Approach
+/**
+ * In the previous solution, even though we were able to bring down the time complexity to O(N), we still needed to do two traversals to find our answer. Can we do this in a single traversal by using smart comparisons on the go?
+
+Approach:
+We would require four variables: small,second_small, large, and second_large. Variable small and second_small are initialized to INT_MAX while large and second_large are initialized to INT_MIN.
+
+Second Smallest Algo:
+
+If the current element is smaller than ‘small’, then we update second_small and small variables
+Else if the current element is smaller than ‘second_small’ then we update the variable ‘second_small’
+Once we traverse the entire array, we would find the second smallest element in the variable second_small.
+Here’s a quick demonstration of the same.
+
+Second Largest Algo:
+
+If the current element is larger than ‘large’ then update second_large and large variables
+Else if the current element is larger than ‘second_large’ then we update the variable second_large.
+Once we traverse the entire array, we would find the second largest element in the variable second_large.
+Here’s a quick demonstration of the same.
+ * 
+ */
+
+function secondSmallest(arr) {
+  if (arr.length < 2) {
+    return -1;
+  }
+  let small = Infinity;
+  let second_small = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < small) {
+      second_small = small;
+      small = arr[i];
+    } else if (arr[i] < second_small && arr[i] !== small) {
+      second_small = arr[i];
+    }
+  }
+  return second_small;
+}
+
+function secondLargest(arr) {
+  if (arr.length < 2) {
+    return -1;
+  }
+  let large = -Infinity;
+  let second_large = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > large) {
+      second_large = large;
+      large = arr[i];
+    } else if (arr[i] > second_large && arr[i] !== large) {
+      second_large = arr[i];
+    }
+  }
+
+  return second_large;
+}
+
+const sS = secondSmallest(arr);
+const sL = secondLargest(arr);
+console.log('Second smallest is ' + sS);
+console.log('Second largest is ' + sL);
+
+//#endregion
