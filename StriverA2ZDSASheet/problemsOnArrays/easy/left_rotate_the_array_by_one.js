5dffb69d0a8e50e59e697f396e3213d5c8c3c549
jpofficials7 <jpofficials7@gmail.com>
2025-08-12 12:14:20 +0530

    left rotate the array by one

---
+++ StriverA2ZDSASheet/problemsOnArrays/easy/left_rotate_the_array_by_one.js
@@ -0,0 +1,38 @@
+/**
+ * Problem Statement: Given an array of N integers, left rotate the array by one place.
+
+Examples
+
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: 2,3,4,5,1
+Explanation: 
+Since all the elements in array will be shifted 
+toward left by one so ‘2’ will now become the 
+first index and and ‘1’ which was present at 
+first index will be shifted at last.
+
+
+Example 2:
+Input: N = 1, array[] = {3}
+Output: 3
+Explanation: Here only element is present and so 
+the element at first index will be shifted to 
+last index which is also by the way the first index.
+ */
+
+function solve(arr) {
+  let temp = new Array(arr.length);
+  for (let i = 1; i < arr.length; i++) {
+    temp[i - 1] = arr[i];
+  }
+
+  temp[arr.length - 1] = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    console.log(temp[i] + ' ');
+  }
+}
+
+let arr = [1, 2, 3, 4, 5];
+solve(arr);
