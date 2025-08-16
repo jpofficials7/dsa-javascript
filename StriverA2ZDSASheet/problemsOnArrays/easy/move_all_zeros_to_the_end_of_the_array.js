14e521b718ca81f9a9d3231e03532bbb64ab175b
jpofficials7 <jpofficials7@gmail.com>
2025-08-16 15:34:30 +0530

    move all zeros to the end of the array optimal approach

--- StriverA2ZDSASheet/problemsOnArrays/easy/move_all_zeros_to_the_end_of_the_array.js
+++ StriverA2ZDSASheet/problemsOnArrays/easy/move_all_zeros_to_the_end_of_the_array.js
@@ -20,3 +20,33 @@ function moveAllZerosToEnd(arr) {
 
 let arr = [1, 0, 2, 3, 2, 0, 0, 4, 5, 1];
 console.log(moveAllZerosToEnd(arr));
+
+// Optimal Solution
+/** The Optimal Solution uses the two pointer approach where i and j , the j will always point to the zero and i will point to the non zero element and we will swap this i and j
+ *
+ * Steps:
+ * 1. Finding the first 0 element
+ * 2. Check the elements next to j if they are non zero
+ */
+
+function moveAllZero(arr) {
+  let j = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      j = i;
+      break;
+    }
+  }
+
+  for (let i = j + 1; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+      j++;
+    }
+  }
+  return arr;
+}
+
+console.log(moveAllZero(arr));
