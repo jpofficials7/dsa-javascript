e4592e5bc328b10bd30c9ea008a257ea3a42b592
jpofficials7 <jpofficials7@gmail.com>
2025-08-19 16:05:52 +0530

    intersection of two sorted arrays

--- StriverA2ZDSASheet/problemsOnArrays/easy/find_the_union_and_intersection_of_two_sorted_arrays.js
+++ StriverA2ZDSASheet/problemsOnArrays/easy/find_the_union_and_intersection_of_two_sorted_arrays.js
@@ -35,3 +35,33 @@ const union = findUnion(arr1, arr2);
 
 console.log('Union of arr1 and arr2 is:');
 console.log(union.join(' '));
+
+// Intersection: The elements that is present in both the arrays. I.e For every element there has to be the corresponding element in the another array
+
+function findIntersection(arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  let ans = [];
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      i++;
+    } else if (arr2[j] < arr1[i]) {
+      j++;
+    } else {
+      ans.push(arr1[i]);
+      i++;
+      j++;
+    }
+  }
+
+  return ans;
+}
+
+const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr4 = [2, 3, 4, 4, 5, 11, 12];
+
+const intersection = findIntersection(arr1, arr2);
+
+console.log('Intersection of arr3 and arr4 is:');
+console.log(intersection.join(' '));
