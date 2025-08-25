65a7a04d4ae62b4e149eafb509c04a5ca784d241
jpofficials7 <jpofficials7@gmail.com>
2025-08-25 16:50:00 +0530

    determine if the input string is valid

--- interviewProblems/determine_if_the_input_string_is_valid.js
+++ interviewProblems/determine_if_the_input_string_is_valid.js
@@ -4,16 +4,31 @@
 // Output: true isValid("(]");
 // Output: false
 
-function isValid(str) {
+/**
+ * You can solve this problem using a stack in JavaScript.
+The idea is:
+
+Push opening brackets ((, {, [) onto the stack.
+
+When a closing bracket (), }, ]) appears, check the top of the stack:
+
+If it matches the corresponding opening bracket, pop it.
+
+If it doesn’t match (or the stack is empty), return false.
+
+At the end, if the stack is empty, the string is valid.
+ */
+
+function isValid(s) {
   let stack = [];
   let map = {
-    '}': '{',
     ')': '(',
+    '}': '{',
     ']': '[',
   };
 
-  for (let char of str) {
-    if (char == '{' || char == '(' || char == '[') {
+  for (let char of s) {
+    if (char === '(' || char === '{' || char === '[') {
       stack.push(char);
     } else {
       if (stack.pop() !== map[char]) {
@@ -21,7 +36,8 @@ function isValid(str) {
       }
     }
   }
-  return str.length === 0;
+
+  return stack.length === 0;
 }
 
 const output = isValid('()[]{}');
