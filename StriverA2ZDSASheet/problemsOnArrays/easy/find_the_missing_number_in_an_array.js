f3d0d26d0a1cc30f56fc88125f013984b2c9a181
jpofficials7 <jpofficials7@gmail.com>
2025-08-27 12:14:48 +0530

    find missing number

--- StriverA2ZDSASheet/problemsOnArrays/easy/find_the_missing_number_in_an_array.js
+++ StriverA2ZDSASheet/problemsOnArrays/easy/find_the_missing_number_in_an_array.js
@@ -10,11 +10,15 @@
 
 // Brute Force Approach
 
-
+/**
+ * In Brute force approach we are trying to iterate the array till n-1 to find the missing number
+ * @param {*} arr
+ * @returns
+ */
 function findMissing(arr) {
-  for (let i = 1; i <= arr.length; i++) {
+  for (let i = 1; i <= n; i++) {
     flag = 0;
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < n - 1; j++) {
       if (arr[j] === i) {
         flag = 1;
         break;
@@ -27,4 +31,44 @@ function findMissing(arr) {
 }
 
 let arr = [1, 2, 3, 5];
-console.log(findMissing(arr));
+let n = 5;
+console.log(findMissing(arr, n));
+
+/**
+ * In Better approach we are trying to find the sum of n natural numbers "s1" and then we are finding the sum 's2" of elements present in the array if s1 and s2 does not match then their difference is the result i.e missing number.
+ * @param {*} arr
+ * @TimeComplexity => O(n)
+ * @SpaceComplexity => O(1)
+ */
+function findMissingBetter(arr, n) {
+  let s1 = (n * (n + 1)) / 2;
+  let s2 = 0;
+  for (let i = 0; i < n - 1; i++) {
+    s2 += arr[i];
+  }
+
+  if (s1 != s2) {
+    return s1 - s2;
+  }
+}
+
+console.log(findMissingBetter(arr, n));
+
+/**
+ * We are XOR for this because XOR of 0 and any number gives the number as the output
+ * @param {*} arr
+ */
+function findMissingOptimal(arr, n) {
+  let xor1 = 0;
+  let xor2 = 0;
+  for (let i = 1; i <= n; i++) {
+    xor1 = xor1 ^ i;
+  }
+  for (let i = 0; i < n - 1; i++) {
+    xor2 = xor2 ^ arr[i];
+  }
+
+  return xor1 ^ xor2;
+}
+
+console.log(findMissingOptimal(arr, n));
