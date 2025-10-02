ef1756ce2018741fae8b1963b3f5251d46f4be23
jpofficials7 <jpofficials7@gmail.com>
2025-10-02 19:42:24 +0530

    two sum problem brute force

---
+++ StriverA2ZDSASheet/problemsOnArrays/medium/1_Two_Sum.js
@@ -0,0 +1,42 @@
+// Two Sum : Check if a pair with given sum exists in Array
+
+/**
+Problem Statement: Given an array of integers arr[] and an integer target.
+
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+Note: You are not allowed to use the same element twice. Example: If the target is equal to 6 and num[1] = 3, then nums[1] + nums[1] = target is not a solution.
+
+Examples:
+
+Example 1:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Result: YES (for 1st variant)
+       [1, 3] (for 2nd variant)
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for the first variant and [1, 3] for 2nd variant.
+
+Example 2:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Result: NO (for 1st variant)
+	[-1, -1] (for 2nd variant)
+Explanation: There exist no such two numbers whose sum is equal to the target.
+*/
+
+function twoSumBruteForce(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) continue;
+      if (arr[i] + arr[j] === target) {
+        return { index1: i, index2: j };
+      }
+    }
+  }
+  return { index1: -1, index2: -1 };
+}
+
+let arr = [2, 6, 5, 6, 11];
+let target = 8;
+const result = twoSumBruteForce(arr, target);
+console.log(result);
